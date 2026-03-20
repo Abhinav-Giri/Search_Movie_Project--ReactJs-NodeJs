0f77b00314f991596747bf9a0207ec6671bec7b5
Abhinav-Giri <iamabhinavgiri@gmail.com>
2026-03-20 16:56:24 +0530

    updated cors url

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const errorHandler = require("./middlewares/globalErrorMiddleware");
 const cors = require("cors");
 require("dotenv").config();
 
@@ -6,7 +7,13 @@ const movieRoutes = require("./routes/movieRoutes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: [
+      "http://localhost:3000",
+      "https://searchmovie-rho.vercel.app/",
+    ],
+    methods: ["GET"],
+  }));
 app.use(express.json());
 
 app.use("/", movieRoutes);
@@ -14,6 +21,7 @@ app.use("/", movieRoutes);
 app.get("/", (req, res) => {
   res.send("OMDB API running...");
 });
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 4200;
 
